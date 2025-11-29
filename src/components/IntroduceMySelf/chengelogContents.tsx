605683150e80715203efc5529871d72f727f8b44
yharuto0917 <yamazakiharuto.09171700@gmail.com>
2025-11-29 13:59:19 +0900

    add second changelog contents

--- src/components/IntroduceMySelf/chengelogContents.tsx
+++ src/components/IntroduceMySelf/chengelogContents.tsx
@@ -1,3 +1,5 @@
+import { title } from "process";
+
 export const changelogContents = [
     {
         tag: [
@@ -6,6 +8,18 @@ export const changelogContents = [
         ],
         date: '2025-12-01',
         title: 'ポートフォリオサイトをリリースしました！',
-        contents: 'ポートフォリオサイトをリリースしました！！[GitHub](https://github.com/yharuto0917)からソースコードをご確認いただけます！'
+        contents: 'ポートフォリオサイトをリリースしました！！[GitHub](https://github.com/yharuto0917)からソースコードをご確認いただけます！',
+        url: 'https://github.com/yharuto0917/ai-powered-profile-site'
+    },
+    {
+        tag: [
+            'Relase',
+            'MCP',
+            'AI Agents'
+        ],
+        date: '2025-12-05',
+        title: 'ThreadsMCPをリリースしました！',
+        contents: 'ThreadsMCPをリリースしました！！[GitHub](https://github.com/yharuto0917)からソースコードをご確認いただけます！',
+        url: 'https://github.com/yharuto0917/ThreadsMCP'
     }
 ]
