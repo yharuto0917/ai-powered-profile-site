48d58b6b3ca7d6f2895a67d2f77ecbfbce0e0f29
yharuto0917 <yamazakiharuto.09171700@gmail.com>
2026-01-22 22:44:46 +0900

    Add changelog about useSubmitJP

--- src/components/IntroduceMySelf/chengelogContents.tsx
+++ src/components/IntroduceMySelf/chengelogContents.tsx
@@ -22,5 +22,16 @@ export const changelogContents = [
         title: 'I release AgentSkills for AI Agent using Google GenAI Python SDK!!',
         contents: 'I am very excited to share the release of AgentSkills for AI Agent using Google GenAI Python SDK. You can check the source code on **[GitHub](https://github.com/yharuto0917/)**. Let\'s build Gemini integration App with Claude Code, Gemini CLI, Codex and so on!!',
         url: 'https://github.com/yharuto0917/GoogleGenAI-AgentSkills-Python'
+    },
+    {
+        tag: [
+            'Release',
+            'React',
+            'Packages'
+        ],
+        date: '2026-1-22',
+        title: 'Send Japanese chats more easily in React Applications!!',
+        contents: 'Today, I introduce useSubmitJP, a new React Hooks for sending chats in the Agentic era!! You can now more easily apply the "Enter + Option key" send function. You are able to use it throughout "npm i use-submit-jp@latest" or "pnpm add use-submit-jp". In addition, you can chack source code or using example in **[GitHub](https://github.com/yharuto0917/useSubmitJP)** !!',
+        url: 'https://www.npmjs.com/package/use-submit-jp'
     }
 ]
