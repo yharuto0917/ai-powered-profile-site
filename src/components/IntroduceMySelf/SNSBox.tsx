49b9c12b273d9a2fe2ca3665af2b95706243cd00
yharuto0917 <yamazakiharuto.09171700@gmail.com>
2026-04-13 00:50:41 +0900

    update: x url

--- src/components/IntroduceMySelf/SNSBox.tsx
+++ src/components/IntroduceMySelf/SNSBox.tsx
@@ -42,7 +42,7 @@ export default function SNSBox() {
                             </Link>
 
                             {/* X (Twitter) */}
-                            <Link href="https://x.com/Yharuto09171700" target="_blank" rel="noopener noreferrer"
+                            <Link href="https://x.com/yharuto917" target="_blank" rel="noopener noreferrer"
                                 className="flex flex-col items-center gap-2 sm:gap-3 group p-3 sm:p-4 rounded-2xl hover:bg-pink-50/50 transition-all duration-300">
                                 <div className="p-2.5 sm:p-3 bg-black/5 rounded-full group-hover:bg-white group-hover:shadow-md transition-all">
                                     <Twitter className="w-7 h-7 sm:w-8 sm:h-8 text-gray-700 group-hover:text-black" />
