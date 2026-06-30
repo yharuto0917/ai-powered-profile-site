7575870e05af0132a52d6a0e8af05289955ce90c
yharuto0917 <yamazakiharuto.09171700@gmail.com>
2026-06-30 23:00:55 +0900

    fix: ChangeLogが表示されないバグを修正
    Framer Motionの whileInView の閾値（amount: 0.4）がChangeLog要素全体の高さに対して大きすぎてビューポートに入らず表示されなかったため、'some' に変更。
    また、chengelogContents で一部 date が data とタイポされていた不具合を修正。

--- src/components/IntroduceMySelf/chengelogContents.tsx
+++ src/components/IntroduceMySelf/chengelogContents.tsx
@@ -8,7 +8,7 @@ export const changelogContents = [
         ],
         date: '2026-1-9',
         title: 'Launch of my new portfolio website!!',
-        contents: 'I am excited to share the launch of my new portfolio website. You can check the source code on **[GitHub](https://github.com/yharuto0917)**.',
+        contents: 'I am excited to share the launch of my new portfolio website. You can check the source code on **[GitHub](https://github.com/yharuto0917)**.\n\n![Launch of Portfolio](/images/changelog/changelog-test.png)',
         url: 'https://github.com/yharuto0917/ai-powered-profile-site'
     },
     {
@@ -33,5 +33,40 @@ export const changelogContents = [
         title: 'Send Japanese chats more easily in React Applications!!',
         contents: 'Today, I introduce useSubmitJP, a new React Hooks for sending chats in the Agentic era!! You can now more easily apply the "Enter + Option key" send function. You are able to use it throughout "npm i use-submit-jp@latest" or "pnpm add use-submit-jp". In addition, you can chack source code or using example in **[GitHub](https://github.com/yharuto0917/useSubmitJP)** !!',
         url: 'https://www.npmjs.com/package/use-submit-jp'
+    },
+    {
+        tag: [
+            'Event',
+            'Hackathon',
+            'Operation',
+            'GDGoC'
+        ],
+        date: '2026-3-25',
+        title: 'We operated Google Developer Groups on Campus Japan Hackathon 2026, GDGoC\'s biggest hackathon in Japan!!',
+        contents: 'We operated Google Developer Groups on Campus Japan Hackathon 2026, GDGoC\'s biggest hackathon for the first time. I joined Marketing Team and managed all marketing work from planning as Core Lead. I also attend final Staff in Google Japan. It was a great experience!! Thanks for all attendees and operators!!',
+        url: 'https://gdsc-jp.connpass.com/event/378883/'
+    },
+    {
+        tag: [
+            'Event',
+            'GoogleI/O',
+            'Developer Conference',
+            'Invited'
+        ],
+        date: '2026-5-19~20',
+        title: 'I was invited to Google I/O 2026 as a Student Developer and it was a great experience!!',
+        contents: 'I was invited to Google I/O 2026 as a Student Developer from Google. I met many developers and learned about the latest trends in AI. Let\'s build Gemini integration App with Google Antigravity 2.0 and Gemini 3.5 Flash!!',
+        url: 'https://io.google/'
+    },
+    {
+        tag: [
+            'Beta-Release',
+            'LLMO',
+            'Google AI Mode'
+        ],
+        date: '2026-6-15',
+        title: 'Introducing LLM-Site Optimizer, an LLM-based site optimization tool!!',
+        contents: 'We are excited to introduce LLM-Site Optimizer, an application that analyzes site to optimize for AI based search platforms like Google AI Mode, ChatGPT Search, Perplexity and so on. Try it now at **[LLM-Site Optimizer](https://llm-site-optimizer.yharuto.dev/)**!!  ',
+        url: 'https://llm-site-optimizer.yharuto.dev/'
     }
 ]
